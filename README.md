7a0fcf865e0351826bd03f7b27c4339a59963a13
Kris <ferg1039star@gmail.com>
2026-07-31 00:22:36 -0500

    Implemented Random dice rolls and updated logic.

--- README.md
+++ README.md
@@ -6,4 +6,9 @@ This first port is a Python text-based adventure game. The initial C# version is
 port from Python as I could make it. Further updates will add features, clean up output, and improve overall
 structure of the system. 
 
+07/31/26
+Implemented Random dice rolls and updated logic for item visibility and win conditions. These dice rolls determine the quality of item the user will see when entering a room
+that contains an item. If the roll exceeds the treshold then the user will be able to equip the higher tier item. This
+update is in line with the intended updates for the optimization of this application.
+
 
--- Text-based Game-Python/Program.cs
+++ Text-based Game-Python/Program.cs
@@ -1,4 +1,6 @@
-﻿Dictionary<string, Dictionary<string, string>> rooms = new()
+﻿using System.Security.Cryptography;
+
+Dictionary<string, Dictionary<string, string>> rooms = new()
 {
     ["Atrium"] = new()
     {
@@ -35,19 +37,60 @@
 };
 
 
-List<string> requiredItems = new()
+Dictionary<string, string> highTier = new()
+{
+    ["Magicians Judge"] = "Belt of Hill Giant Strength", ["Helm of Larethian Wit"] = "Dawnbringer",
+    ["Orbum Misticum"] = "Efreeti Chain",
+    ["Armor of Agathys"] = "Pyxis of Pandemonium", ["Shield of the Morning"] = "Shard of Solitaire",
+    ["Boots of Haste"] = "Witchlight Watch"
+};
+
+
+List<string> possibleMagicItemsLowTier = new()
 {
     "Boots of Haste", "Magicians Judge", "Helm of Larethian Wit", "Orbum Misticum", "Armor of Agathys", "Shield of the Morning"
 };
 
 
+List<string> possibleMagicItemsHighTier = new()
+{
+    "Belt of Hill Giant Strength", "DawnBringer", "Efreeti Chain", "Pyxis of Pandemonium", "Shard of Solitaire", "Witchlight Watch"
+};
+
 List<string> inventory = new();
 
 
-
-
-
 string currentRoom = "Atrium";
+int currentRoomRoll = 0;
+
+
+int diceRoll()
+{
+    int d20 = RandomNumberGenerator.GetInt32(1, 21);
+    bool isNatural20 = false;
+    bool isNatural1 = false;
+    
+
+    if (d20 == 20)
+    {
+        isNatural20 = true;
+        Console.WriteLine($"Your d20 roll is {d20}: Critical Success!");
+        return d20;
+    }
+    else if (d20 == 1)
+    {
+        isNatural1 = true;
+        Console.WriteLine($"Your d20 roll is {d20}: Critical Failure!");
+        return d20;
+    }
+    else
+    {
+        Console.WriteLine($"Your d20 roll is {d20}");
+        return d20;
+    }
+}
+
+
 
 
 static void ShowInstructions()
@@ -61,7 +104,7 @@ static void ShowInstructions()
     Console.WriteLine(new string('-', 68));
 }
 
-static void ShowStatus(string currentRoom, List<string> inventory, Dictionary<string, Dictionary<string, string>> rooms)
+static void ShowStatus(string currentRoom, List<string> inventory, Dictionary<string, Dictionary<string, string>> rooms, Dictionary<string, string> highTier, int currentRoomRoll)
 {
     Console.WriteLine(new string('*', 35));
     Console.WriteLine($"You are in the {currentRoom}");
@@ -69,7 +112,15 @@ static void ShowStatus(string currentRoom, List<string> inventory, Dictionary<st
     if (rooms[currentRoom].ContainsKey("Item") && !string.IsNullOrEmpty(rooms[currentRoom]["Item"]) && rooms[currentRoom]["Item"] != "no items")
     {
         string item = rooms[currentRoom]["Item"];
-        Console.WriteLine($"You see the {item}");
+        highTier.TryGetValue(item, out string highTierItem);
+        if (currentRoomRoll >= 15)
+        {
+            Console.WriteLine($"Your investigation was high! You see the {highTierItem}");
+        }
+        else
+        {
+            Console.WriteLine($"You see the {item}");
+        }
     }
     else
     {
@@ -83,7 +134,7 @@ static void ShowStatus(string currentRoom, List<string> inventory, Dictionary<st
 while (true)
 {
     ShowInstructions();
-    ShowStatus(currentRoom, inventory, rooms);
+    ShowStatus(currentRoom, inventory, rooms, highTier, currentRoomRoll);
     Console.Write("What would you like to do?\n>");
     string? userAction = Console.ReadLine()?.Trim().ToLower();
     Console.WriteLine(userAction);
@@ -106,6 +157,8 @@ while (true)
 
             if (rooms[currentRoom].ContainsKey(direction))
             {
+                currentRoomRoll = diceRoll();
+                Console.WriteLine($"Your investigation check was {currentRoomRoll}");
                 currentRoom = rooms[currentRoom][direction];
                 Console.WriteLine($"You move to {currentRoom}");
             }
@@ -131,17 +184,44 @@ while (true)
             if (rooms[currentRoom].ContainsKey("Item") && !string.IsNullOrEmpty(rooms[currentRoom]["Item"]) &&
                 rooms[currentRoom]["Item"] != "no items")
             {
-                string actualItem = rooms[currentRoom]["Item"];
-                if (string.Equals(requestedItem, actualItem, StringComparison.OrdinalIgnoreCase))
+                string baseLineItem = rooms[currentRoom]["Item"];
+                
+                if (currentRoomRoll < 15)
                 {
-                    inventory.Add(actualItem);
-                    rooms[currentRoom]["Item"] = "";
-                    Console.WriteLine($"the {actualItem} is equipped, you feel its power coursing through you");
+                    
+                    if (string.Equals(requestedItem, baseLineItem, StringComparison.CurrentCultureIgnoreCase))
+                    {
+
+                        inventory.Add(baseLineItem);
+                        rooms[currentRoom]["Item"] = "";
+                        Console.WriteLine(
+                            $"the {baseLineItem} is equipped, you feel a slight surge of power coursing through you");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You didn't type the item correctly, try again");
+                    }
+                }
+                else if (currentRoomRoll >= 15)
+                {
+                    highTier.TryGetValue(baseLineItem, out string highTierItem);
+                    if (string.Equals(requestedItem, highTierItem, StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        inventory.Add(highTierItem);
+                        rooms[currentRoom]["Item"] = "";
+                        Console.WriteLine(
+                            $"the {highTierItem} is equipped, you feel an overwhelming rush of power coursing through you");
+                        
+                    }
+                    else
+                    {
+                        Console.WriteLine("You didn't type the item correctly, try again");
+                        continue;
+                    }
                 }
                 else
                 {
-                    Console.WriteLine("You rolled a Natural 1 for investigation, try again");
-                    continue;
+                    Console.WriteLine("You rolled a Natural 1 for investigation, try again.");
                 }
             }
             else
@@ -158,18 +238,26 @@ while (true)
 
     if (currentRoom == "Crypt")
     {
+        int lowTierCount = 0;
+        int highTierCount = 0;
+        
+        
         bool hasAllItems = true;
 
-        foreach (string needed in requiredItems)
+        foreach (string needed in inventory)
         {
-            if (!inventory.Contains(needed))
+            if (possibleMagicItemsLowTier.Contains(needed))
             {
-                hasAllItems = false;
-                break;
+                lowTierCount++;
+                
+            }
+            else if (possibleMagicItemsHighTier.Contains(needed))
+            {
+                highTierCount++;
             }
         }
 
-        if (!hasAllItems)
+        if ((highTierCount < 4 && lowTierCount < 6))
         {
             Console.WriteLine("Here lies our intrepid adventurer, who thought they could slay Ferg-the-licious without all the help they could get");
             Console.WriteLine("Game Over");
@@ -198,6 +286,6 @@ while (true)
  * -add health potions
  * -add more details for room desriptions
  * -add option for perception or investigation check with an associated 'dice roll'
- * -item in room will depend on both success of dice roll, and type of dice roll
+ * -item in room will depend on both success of dice roll, and type of dice roll **implemented partially 07/31/26
  */
 
